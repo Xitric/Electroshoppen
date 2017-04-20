a4d711cc1c71608d2fe5508bed0ad4f5018aa94d
Mikkel Struntze <mikkel.struntze@gmail.com>
2017-04-20 12:49:39 +0200

    Attribute and category managers (not implemented)

--- src/pim/business/Attribute.java
+++ src/pim/business/Attribute.java
@@ -49,7 +49,7 @@ public class Attribute implements Comparable<Attribute> {
 
 	/**
 	 * Constructs a new attribute with the specified name and list of legal values. If all values are allowed, use
-	 * {@link #Attribute(String, String, Object)} instead, or pass null as the parameter.
+	 * {@link #Attribute(String, String, Object)} instead, or pass null as the 4th parameter.
 	 *
 	 * @param id           the id of the attribute
 	 * @param name         the name of the attribute
