b209ad78613c56db3c305567b3b0c8b086d36c2f
Xitric <KasperSchultzDavidsen@gmail.com>
2017-04-21 12:58:25 +0200

    Added selector for chars and JavaFX colors Resolved a number of TODO's

--- src/pim/presentation/CreateAttributeDialog.java
+++ src/pim/presentation/CreateAttributeDialog.java
@@ -7,20 +7,30 @@ import javafx.collections.SetChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.Arrays;
 import java.util.ResourceBundle;
 
 /**
+ * Controller for the dialog responsible for gathering data for creating new attributes.
+ *
  * @author Kasper
  */
 public class CreateAttributeDialog implements Initializable {
+
+	/**
+	 * The data types that are supported by this dialogue window.
+	 */
+	private static final String[] dataTypes = {"String", "Character", "Integer", "Color"};
 
 	@FXML
 	private TextField nameField;
@@ -47,15 +57,23 @@ public class CreateAttributeDialog implements Initializable {
 	@FXML
 	private BorderPane valueSelectionPane;
 
+	/**
+	 * The set of legal values currently selected.
+	 */
 	private ObservableSet<Object> legalValues;
 
+	/**
+	 * The current control for selecting attribute values. Varies with the selected data type.
+	 */
 	private ValueSelector currentSelector;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		//Listen to changes in the selected data type
 		dataTypeComboBox.getSelectionModel().selectedItemProperty().addListener(this::dataTypeChanged);
 
 		//See: http://stackoverflow.com/questions/33901891/binding-a-listview-to-an-observableset
+		//Make the list view of legal values display a 'set' of objects
 		legalValues = FXCollections.observableSet();
 		legalValues.addListener((SetChangeListener.Change<?> c) -> {
 			if (c.wasAdded()) {
@@ -65,32 +83,37 @@ public class CreateAttributeDialog implements Initializable {
 			}
 		});
 
-		//TODO: START Temp
-		dataTypeComboBox.setItems(FXCollections.observableArrayList("String", "Color", "Integer", "Character"));
-		//TODO: END Temp
+		//Populate combo box for data types
+		dataTypeComboBox.setItems(FXCollections.observableArrayList(dataTypes));
+		dataTypeComboBox.getSelectionModel().select(0);
 	}
 
 	private void dataTypeChanged(Observable observable) {
-		//TODO: Set value selection
-		//TODO: START Temp
+		//Depending on the selected data type, set the current value selector
+		ValueSelector vs = null;
+
 		switch (dataTypeComboBox.getSelectionModel().getSelectedItem()) {
 			case "String":
-				StringValueSelector svs = new StringValueSelector();
-				currentSelector = svs;
-				valueSelectionPane.setCenter(svs);
+				vs = new StringValueSelector();
+				break;
+			case "Character":
+				vs = new CharValueSelector();
 				break;
 			case "Integer":
-				IntegerValueSelector ivs = new IntegerValueSelector();
-				currentSelector = ivs;
-				valueSelectionPane.setCenter(ivs);
+				vs = new IntegerValueSelector();
 				break;
-			default:
-				currentSelector = null;
-				valueSelectionPane.setCenter(null);
+			case "Color":
+				vs = new ColorValueSelector();
+				break;
 		}
 
-		valueSelectionPane.getScene().getWindow().sizeToScene();
-		//TODO: END Temp
+		//The value of vs should not be null, or we have made a programming error
+		currentSelector = vs;
+		valueSelectionPane.setCenter((Node) vs); //This should be safe
+
+		//We only resize the window if the content had been added to a dialog window
+		if (valueSelectionPane.getScene() != null)
+			valueSelectionPane.getScene().getWindow().sizeToScene(); //Resize to fit content
 
 		//Clear all values when data type changes
 		defaultValueField.clear();
@@ -115,28 +138,25 @@ public class CreateAttributeDialog implements Initializable {
 
 	@FXML
 	private void addDefaultButtonOnAction(ActionEvent event) {
-		//TODO: No need to handle null selector in final code
-		if (currentSelector == null) return;
-
 		Object[] values = currentSelector.getValues();
 		if (values.length > 0) {
 			//By convention, only the first value is added as default
 			Object value = values[0];
 
-			//Set default only if value is among legal values, or if values are unrestricted
-			if (!restrictedCheckBox.isSelected() || legalValues.contains(value)) {
-				defaultValueField.setText(value.toString());
-
-				//TODO: Save object
+			//If values are restricted, add value to legal values
+			if (restrictedCheckBox.isSelected()) {
+				legalValues.add(value);
 			}
+
+			//Set default value
+			defaultValueField.setText(value.toString());
+
+			//TODO: Save object
 		}
 	}
 
 	@FXML
 	private void addLegalButtonOnAction(ActionEvent event) {
-		//TODO: No need to handle null selector in final code
-		if (currentSelector == null) return;
-
 		legalValues.addAll(Arrays.asList(currentSelector.getValues()));
 	}
 
@@ -183,6 +203,38 @@ public class CreateAttributeDialog implements Initializable {
 
 			//Otherwise return single valued array
 			return new String[]{textField.getText()};
+		}
+	}
+
+	/**
+	 * Describes a value selector for a single character.
+	 */
+	private class CharValueSelector extends HBox implements ValueSelector<Character> {
+
+		private TextField textField;
+
+		private CharValueSelector() {
+			super();
+
+			setSpacing(8);
+
+			//Add label
+			getChildren().add(new Label("Value:"));
+
+			//Add text field
+			textField = new TextField();
+			HBox.setHgrow(textField, Priority.ALWAYS);
+			getChildren().add(textField);
+		}
+
+		@Override
+		public Character[] getValues() {
+			//If nothing or too much is written, return empty array
+			if (textField.getText().length() == 0 || textField.getText().length() > 1) return new Character[0];
+
+			//Otherwise return single valued array. This is safe because we have ensured that the size is exactly 1
+			//(negative lengths not possible)
+			return new Character[]{textField.getText().charAt(0)};
 		}
 	}
 
@@ -263,5 +315,27 @@ public class CreateAttributeDialog implements Initializable {
 		}
 	}
 
-	//TODO: More selectors
+	/**
+	 * Describes a value selector for a single string.
+	 */
+	private class ColorValueSelector extends HBox implements ValueSelector<Color> {
+
+		private ColorPicker colorPicker;
+
+		private ColorValueSelector() {
+			super();
+
+			setAlignment(Pos.CENTER);
+
+			//Add color picker
+			colorPicker = new ColorPicker();
+			getChildren().add(colorPicker);
+		}
+
+		@Override
+		public Color[] getValues() {
+			//The color picker ensures that something is always selected
+			return new Color[]{colorPicker.getValue()};
+		}
+	}
 }
