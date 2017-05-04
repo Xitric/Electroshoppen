d7a64d8feae73131e2b324eae140004147f184db
Xitric <KasperSchultzDavidsen@gmail.com>
2017-05-04 12:22:40 +0200

    RemoveableImage now honors the aspect ratio of the image

--- src/pim/presentation/ProductController.java
+++ src/pim/presentation/ProductController.java
@@ -5,6 +5,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.layout.FlowPane;
 import pim.business.Image;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,24 +22,14 @@ public class ProductController implements Initializable {
 	 */
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		Image img = new Image("res/omen-ax005no.jpg");
-		productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
-		productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
-		productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
-		productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
-		productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
-		productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
-		productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
-		productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
-		productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
-		productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
-		productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
-		productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
-		productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
-		productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
-		productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
-		productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
-		productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
+		File dir = new File("res");
+		for (File file: dir.listFiles()) {
+			if (file.getName().equals("cross.png")) {
+				continue;
+			}
+			Image img = new Image(file.getAbsolutePath());
+			productImagePane.getChildren().add(new RemoveableImage(img.getImage(), this::removeImage));
+		}
 	}
 
 	private void removeImage(RemoveableImage img) {
