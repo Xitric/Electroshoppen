c26703fb73b4b529a6447bc22c2af119766adcc0
em135 <emilqz@gmail.com>
2017-05-22 09:35:14 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/pim/presentation/PIMController.java

--- src/webshop/business/Webshop.java
+++ src/webshop/business/Webshop.java
@@ -4,32 +4,5 @@ package webshop.business;
  * @author Emil
  */
 public class Webshop {
-/*
-webshop:
-visProdukter();
-tilføjProdukt(produkt);
-tjekUd();
-angivLeveringsoplysninger();
-angivBetalingsoplysninger();
-seProduktListe(); //kald CMS med produktListe
 
-//filtrerKategori(kategori)
-//filtrerAttribut(attribut)
-//filtrerTag(tag)
-
-kurv:
-tilFøjProruktTilKurv(produkt)
-
-kunde:
-opretOdre();
-sætLeveringsoplysninger();
-sætBetalingsoplysninger();
-
-Ordre
-opret(kurv)
-sætLeveringsoplysninger();
-sætBetalingsoplysninger();
-færdiggør();
-
- */
 }
