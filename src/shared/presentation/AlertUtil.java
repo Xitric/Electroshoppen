fead631cf8f52e6166a03a621f56b75516648b9e
MemmoSJ <Mads28@live.dk>
2017-05-24 09:46:32 +0200

    eh.

--- src/shared/presentation/AlertUtil.java
+++ src/shared/presentation/AlertUtil.java
@@ -36,4 +36,51 @@ public class AlertUtil {
 		alert.setContentText(content);
 		return alert;
 	}
+
+	/**
+	 * Constructs a new alert of any type, depending on the arguments given.
+	 * The alert is populated by the corresponding String arguments
+	 *
+	 * @param type		The type of alert
+	 * @param title		The title text of the alert dialog
+	 * @param header	The header text of the alert dialog
+	 * @param content	The content text of the alert dialog
+	 * @return	Returns an alert dialog using the arguments given
+	 */
+	public static Alert newAlertDialog(Alert.AlertType type, String title, String header,String content) {
+		Alert alert;
+		switch (type) {
+			case CONFIRMATION:
+				alert = new Alert(Alert.AlertType.CONFIRMATION);
+				alert.setTitle(title);
+				alert.setHeaderText(header);
+				alert.setContentText(content);
+				break;
+			case ERROR:
+				alert = new Alert(Alert.AlertType.ERROR);
+				alert.setTitle(title);
+				alert.setHeaderText(header);
+				alert.setContentText(content);
+				break;
+			case WARNING:
+				alert = new Alert(Alert.AlertType.WARNING);
+				alert.setTitle(title);
+				alert.setHeaderText(header);
+				alert.setContentText(content);
+				break;
+			case INFORMATION:
+				alert = new Alert(Alert.AlertType.INFORMATION);
+				alert.setTitle(title);
+				alert.setHeaderText(header);
+				alert.setContentText(content);
+				break;
+			default:
+				alert = new Alert(Alert.AlertType.NONE);
+				alert.setTitle(title);
+				alert.setHeaderText(header);
+				alert.setContentText(content);
+				break;
+		}
+		return alert;
+	}
 }
