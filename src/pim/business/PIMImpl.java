ff7a2df5cd23cea4b9059ad7ec4d3cfd512d1b82
MemmoSJ <Mads28@live.dk>
2017-05-24 11:29:38 +0200

    eh.

--- src/pim/business/PIMImpl.java
+++ src/pim/business/PIMImpl.java
@@ -179,7 +179,6 @@ class PIMImpl implements PIM {
 
 	@Override
 	public List<Category> getCategoriesWithAttribute(int attributeID) throws IOException {
-		//TODO
 		List<Category> categories = new ArrayList<>();
 		for (Category category : categoryManager.getCategories()) {
 			for (Attribute attribute : category.getAttributes()) {
