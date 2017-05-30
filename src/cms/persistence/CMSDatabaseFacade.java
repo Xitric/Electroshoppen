8d218efc2d26da7533e2d18b98256ebe3a05cbed
Xitric <KasperSchultzDavidsen@gmail.com>
2017-05-30 18:31:23 +0200

    Local database

--- src/cms/persistence/CMSDatabaseFacade.java
+++ src/cms/persistence/CMSDatabaseFacade.java
@@ -23,7 +23,7 @@ class CMSDatabaseFacade implements CMSPersistenceFacade {
 
 	/* Variables for database connection */
 //	private final static String url = "jdbc:postgresql://46.101.142.251:5432/cms";
-	private final static String url = "jdbc:postgresql://localhost:5432/CMS";
+	private final static String url = "jdbc:postgresql://localhost:5432/ElectroshopLocalTestCMS";
 	private final static String user = "postgres";
 	private final static String password = "1234";
 
